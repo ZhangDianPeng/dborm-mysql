78fd1914c4cd1ec4c2656abc2dc816355d91b636
过靖康 <guojingkang@corp.netease.com>
2026-04-03 18:01:33 +0800

    更新：ECONNRESET 连接异常处理

--- base/db.js
+++ base/db.js
@@ -40,7 +40,7 @@ module.exports = (dbConfig, {log, noConvertDbCodes, dbCode, logExecuteTime, logg
         return new Promise(function (resolve, reject) {
             db.pool.getConnection(function (err, connection) {
                 if (err) {
-                    if(err.code === 'ECONNREFUSED' || err.code === 'ETIMEDOUT' || err.code === 'PROTOCOL_SEQUENCE_TIMEOUT'){
+                    if(err.code === 'ECONNREFUSED' || err.code === 'ETIMEDOUT' || err.code === 'ECONNRESET' || err.code === 'PROTOCOL_SEQUENCE_TIMEOUT'){
                         logger('mysql reconnect， reconnect time:', reconnectionTime++);
                         db.getConnection().then(resolve, reject);
                     }
